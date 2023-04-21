4fc57a3c56ace6498cc3524f88d3e7eacf4e4a5f
alanxue1 <130785943+alanxue1@users.noreply.github.com>
2023-04-20 23:17:37 -0400

    start of scene 3a & 3b
    -finished the if statements for scene 2
    -created methods for every single scene
    -finished scene 3a and 3b excluding images and animations

--- MidtermProject.java
+++ MidtermProject.java
@@ -15,7 +15,6 @@ public class MidtermProject{
 		double dblDifficulty;
 		int intHealth = 100;
 		int intDoors;
-		int intSpeed;
 		String strAnswer;
 		int intVowels;
 		char chrChoice;
@@ -26,7 +25,7 @@ public class MidtermProject{
 		con.setTextFont(fntArcade);
 		BufferedImage imgScene1 = con.loadImage("scene1.png");
 		con.drawImage(imgScene1,-200,-200);
-		
+		/*
 		con.repaint();
 		con.println("-------------------------------------------------------------------------------------------------------------------------------------");
 		con.sleep(300);
@@ -68,7 +67,7 @@ public class MidtermProject{
 		con.sleep(330);
 		con.println("-------------------------------------------------------------------------------------------------------------------------------------");
 		con.sleep(2300);
-		
+		*/
 		intLuck = (int)(Math.random()*10+1);
 		con.println("");
 		con.println("Your luck is "+intLuck);
@@ -92,7 +91,7 @@ public class MidtermProject{
 			}
 		con.println("Choose difficulty level (1.0 - easy, 2.0 - medium, 3.0 - hard)");
 		while(true){
-			dblDifficulty = con.readInt();
+			dblDifficulty = con.readDouble();
 			if(dblDifficulty == 1.0){
 				intLuck = intLuck;
 				con.println("int luck stays the same "+intLuck);
@@ -111,23 +110,47 @@ public class MidtermProject{
 		}
 		con.println("Good luck");
 		con.sleep(2600);
-		 
-		scene2(con);
+		
+		boolean sucess = scene2(con,intLuck, intHealth);
+		boolean scene3aResult;
+		boolean scene3bResult;
+		if(sucess) {
+			scene3aResult = scene3a(con,intHealth,intLuck);
+			if(scene3aResult == true){
+				scene4a(con);
+			}else{
+				scene4b(con);
+			}
+		}
+		
+		else{
+			scene3bResult = scene3b(con,intHealth, intLuck);
+			if(scene3bResult == true){
+				scene4a(con);
+			}else{
+				scene4b(con);
+			}
+			
+		}
+	
+		
 
 
 		
 	}
-	public static void scene2(Console con) {
+	public static boolean scene2(Console con, int intLuck, int intHealth) {
     con.clear();
     BufferedImage imgPrison = con.loadImage("prison.jpg");
     con.drawImage(imgPrison, 0, 0);
     con.setTextColor(Color.WHITE);
+    /*
     con.println("You wake up groggily, your head throbbing from the blow that knocked you unconscious.");
     con.sleep(2300);
     con.println("As your eyes adjust to the dim light, you realize that you are in a small cell with a heavy iron door.");
     con.sleep(2300);
     con.println("A guard is now standing inside, keeping watch.");
     con.sleep(2000);
+    */ 
     con.println("Press either 'a'(attack guard) or 'b'(bribe guard) or 'c'(charm guard) on your keyboard");
 
     int intGuard = 300;
@@ -156,27 +179,193 @@ public class MidtermProject{
 		con.sleep(33);
        char currentChar = con.currentChar();
         if (Character.isLowerCase(currentChar)) {
-            // Handle user input for protagonist's actions based on currentChar
+
             if (currentChar == 'a') {
-                con.println("Protagonist tries to attack the guard.");
-                break;
-                // Add animation for attack action
+                con.println("You try to attack the guard.");
+                
+                if(intLuck>= 5){
+					con.sleep(2300);
+					con.println("You knock out the guard and take his keys.");
+					con.sleep(2300);
+					return true;
+				}else if(intLuck<5){
+					con.sleep(2300);
+					con.println("The guard overpowers you and brings you to the torture chamber");
+					intHealth -= 50;
+					con.sleep(2300);
+					return false;
+			}
+
             } else if (currentChar == 'b') {
-                con.println("Protagonist tries to bribe the guard.");
-                break;
-                // Add animation for bribe action
+                con.println("You try to bribe the guard.");
+                if(intLuck>= 5){
+					con.sleep(2300);
+					con.println("The guard takes your money and sets you free");
+					con.sleep(2300);
+					return true;
+				}else if(intLuck<5){
+					con.sleep(2300);
+					con.println("The guard does not care about your insignificant bribe and brings you to the torture chamber.");
+					intHealth -= 50;
+					con.sleep(2300);
+					return false;
+				}
+
             } else if (currentChar == 'c') {
-                con.println("Protagonist tries to charm the guard.");
-                break;
-                // Add animation for charm action
-            } else {
-                con.println("Protagonist does nothing.");
-                // Add animation for no action
+				con.println("You try to charm the guard.");
+                if(intLuck>= 5){
+					con.sleep(2300);
+					con.println("You charmed the guard and he sets you free");
+					con.sleep(2300);
+					return true;
+				}else if(intLuck<5){
+					con.sleep(2300);
+					con.println("The guard is unflattered with your charms and decides you bring you to the torture chamber.");
+					intHealth -= 50;
+					con.sleep(2300);
+					return false;
+				}
             }
         }
 
     }
-
 }
 
+public static boolean scene3a(Console con, int intHealth, int intLuck){
+	con.clear();
+	BufferedImage imgPrison = con.loadImage("prison.jpg");
+    con.drawImage(imgPrison, 0, 0);
+	con.println("The Corridor");
+	con.println("How many doors do you want to open?");
+	int intDoors = con.readInt();
+	con.sleep(1000);
+	for(int intCount= 0;intCount<intDoors;intCount++){
+		int intItem = (int)(Math.random()*9)+1;
+		if(intItem == 2){
+			con.println("You found food");
+			intHealth += 10;
+			con.println("Your health has been increased by 10, it is now: "+intHealth);
+			con.sleep(1000);
+		}else if(intItem == 4){
+			con.println("You found armour");
+			intHealth += 10;
+			con.println("Your health has been increased by 10, it is now: "+intHealth);
+			con.sleep(1000);
+		}else if(intItem == 6){
+			con.println("You found a potion");
+			intHealth += 10;
+			con.println("Your health has been increased by 10, it is now: "+intHealth);
+			con.sleep(1000);
+		}else if(intItem == 8){
+			con.println("You found water");
+			intHealth += 10;
+			con.println("Your health has been increased by 10, it is now: "+intHealth);
+			con.sleep(1000);
+		}else if(intItem == 10){
+			con.println("You found bandaid");
+			intHealth += 10;
+			con.println("Your health has been increased by 10, it is now: "+intHealth);
+			con.sleep(1000);
+		}else if(intItem == 1){
+			con.println("You ran into a poisonous snake");
+			intHealth -= 10;
+			con.println("Your health has been decreased by 10, it is now: "+intHealth);
+			con.sleep(1000);
+		}else if(intItem == 3){
+			con.println("You ran into a trap");
+			intHealth -= 10;
+			con.println("Your health has been decreased by 10, it is now: "+intHealth);
+			con.sleep(1000);
+		}else if(intItem == 5){
+			con.println("A witch has cursed you");
+			intHealth -= 10;
+			con.println("Your health has been decreased by 10, it is now: "+intHealth);
+			con.sleep(1000);
+		}else if(intItem == 7){
+			con.println("You tripped on a rock");
+			intHealth -= 10;
+			con.println("Your health has been decreased by 10, it is now: "+intHealth);
+			con.sleep(1000);
+		}else if(intItem == 9){
+			con.println("You got bit by a spider");
+			intHealth -= 10;
+			con.println("Your health has been decreased by 10, it is now: "+intHealth);
+			con.sleep(1000);
+		}
+	}
+	con.clear();
+	con.sleep(1000);
+	con.println("REMINDER your luck is: "+intLuck);
+	con.sleep(1000);
+	con.println("In order to escape from the guards chasing you, you must solve this problem");
+	con.sleep(1000);
+	con.println("Your speed + luck must be equal to 5, what is the minimum speed you need");
+	double dblAnswer = 5 - intLuck;
+	double dblSpeed = con.readDouble();
+	if(dblAnswer == dblSpeed){
+		con.println("Congratulations, you have successfully escaped from the guards");
+		con.sleep(3000);
+		return true;
+	}else{
+		con.println("You have been caught and now the king is waiting for your execution");
+		con.sleep(3000);
+		return false;
+	}
 }
+	
+public static boolean scene3b(Console con,int intHealth, int intLuck){
+	con.clear();
+	//BufferedImage imgPrison = con.loadImage("prison.jpg");
+    //con.drawImage(imgPrison, 0, 0);
+	con.println("The Torture Chamber");
+	con.println("What do you want to say to the interrogator");
+	String strTorture = con.readLine();
+	String strLetter;
+	int intLength = strTorture.length();
+	int intVowel = 0;
+
+		for(int intCount = 0; intCount < intLength; intCount++){
+			strLetter = strTorture.substring(intCount, intCount +1);
+			//con.println("TEST: "+strLetter);
+			if(strLetter.equals("a")||strLetter.equals("e")||strLetter.equals("i")||strLetter.equals("o")||strLetter.equals("u")||strLetter.equals("y")){
+				intVowel = intVowel +1;
+			}
+		}
+		if(intVowel>=5){
+			con.println("The interrogator believes you and sets you free");
+			con.sleep(1000);
+			return true;
+		}else{
+			con.println("The interrogator does not believe you and now the king is waiting for your execution");
+			con.sleep(1000);
+			return false;
+		}
+	}
+public static void scene4a(Console con){
+	con.clear();
+	BufferedImage imgPrison = con.loadImage("prison.jpg");
+    con.drawImage(imgPrison, 0, 0);
+	con.println("The Courtyard");
+	}
+public static void scene4b(Console con){
+	con.clear();
+	BufferedImage imgPrison = con.loadImage("prison.jpg");
+    con.drawImage(imgPrison, 0, 0);
+	con.println("The Execution");
+	}
+public static void scene5a(Console con){
+	con.clear();
+	BufferedImage imgPrison = con.loadImage("prison.jpg");
+    con.drawImage(imgPrison, 0, 0);
+	con.println("The Freedom");
+	}
+public static void scene5b(Console con){
+	con.clear();
+	BufferedImage imgPrison = con.loadImage("prison.jpg");
+    con.drawImage(imgPrison, 0, 0);
+	con.println("The Death");
+	}
+}
+
+
+
